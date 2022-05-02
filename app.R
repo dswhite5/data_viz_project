ecbc9735a0d0529550146f1974993df43f38279e
dswhite5 <whiteds@mail.gvsu.edu>
2022-05-02 16:51:14 -0400

    update app

--- app.R
+++ app.R
@@ -1,1 +1,23 @@
 #data Viz project
+#shiny app
+library(shiny)
+library(tidyverse)
+indicators <- read_csv('C:/Users/sethh/OneDrive/Documents/CIS 671/Semester Project/data/Indicators.csv')
+
+#user interface
+ui <- fluidPage(
+  
+
+)
+
+
+# server logic
+
+server <- function(input, output) {
+  
+}
+
+
+#run the shiny app
+
+shinyApp(ui = ui, server = server)
