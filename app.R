644612135d51e6a48ad7c97c5112c162211e6f70
dswhite5 <whiteds@mail.gvsu.edu>
2022-05-04 10:13:27 -0400

    add www for photos, and progrees on app layout and outline

--- app.R
+++ app.R
@@ -3,25 +3,55 @@
 #library(shiny)
 #library(tidyverse)
 
-source("Data and Visualization Exploration.R")
+#source("Data and Visualization Exploration.R")
 
 #user interface
-ui <- navbarPage("Title of the page",
+ui <- navbarPage("Data Visualization Strategies",
+                 #tab panel for the first page
                  tabPanel("Introduction",
-                          fluidPage(titlePanel(h1("This is a title")))
-                          ),
-                 tabPanel("Tasks",
-                          fluidPage(titlePanel(h1("This is the second page")),
-                                    mainPanel(h3("Pargraph goes here")
+                          fluidPage(titlePanel(h1("Data Visualization Strategies")),
+                                    mainPanel(h1('Project overview'),
+                                              h3("This will disucss the page layout and what the data visualization stategies are"),
+                                              h1('Task Oriented Design'),
+                                              h3('Paragraph on task oriented design'),
+                                              h1('World Indicators Data'),
+                                              h3('This paragraph will discuss the data and talk about the famous example using this data'),
+                                              img(src = 'indicators_data_set.PNG', height = 400, width = 800),
+                                              h1('note on statistical methods'),
+                                              h3('These are not rigorous statistical methods. That may be added at a later date')
                                               )
                                     )
                           ),
+                 #tab panel for visual encoding page
                  tabPanel("Visual Encodings",
-                          fluidPage(titlePanel(h1("This is the third page")),
-                                    mainPanel(h3("Pargraph goes here")
+                          fluidPage(titlePanel(h1("Visual encodings and Visualization Strategies")),
+                                    mainPanel(h1("Overview"),
+                                              h3("What are visual encodings, gestalt principles and clevelands rules"),
+                                              h1('Visual Encodings'),
+                                              h3('what are visual encodings'),
+                                              h1("Cleveland's Rules"),
+                                              h3('What are clevelands Rules'),
+                                              h1('Gestalts Principles'),
+                                              h3('What are Gestalts Rpinciples'),
+                                              br(),
+                                              h3('Maybe show some examples?')
+                                              
                                               )
                                     )
                           ),
+                 #panel for tasks page
+                 tabPanel("Tasks",
+                          fluidPage(titlePanel(h1("Tasks to Accomplish")),
+                                    mainPanel(h1("Task 1"),
+                                              h3("Task 1 goes here"),
+                                              h1('Task 2'),
+                                              h3('Task 2 goes here'),
+                                              h1('Task 3'),
+                                              h3('Task 3 goes here')
+                                    )
+                          )
+                 ),
+                 #panel for graphs page
                  tabPanel("Graphs",
                           fluidPage(titlePanel(h1("This Page shows graphs")),
                                     mainPanel(h3("Pargraph goes here"),
